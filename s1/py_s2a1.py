a3caacd976b53866d58131746d8cc2473a4f50cb
Ajay51 <iajaypatil.ap@gmail.com>
2026-01-06 19:17:31 +0530

    commiting first assignment

---
+++ s1/py_s2a1.py
@@ -0,0 +1,8 @@
+x=int(input("Enter your first number: "))
+y=int(input("Enter your second number: "))
+temp=x
+temp2=y
+x=temp2
+y=temp
+print("After swapping first number is:",x)
+print("After swapping second number is:",y)
