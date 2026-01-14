d1df9cb9eb610d8cb1318b47972f389000838c66
Ajay51 <iajaypatil.ap@gmail.com>
2026-01-14 17:57:29 +0530

    name change and s2a1

--- s1/py_s2a1.py
+++ s1/py_s2a1.py
@@ -1,8 +1,4 @@
-x=int(input("Enter your first number: "))
-y=int(input("Enter your second number: "))
-temp=x
-temp2=y
-x=temp2
-y=temp
-print("After swapping first number is:",x)
-print("After swapping second number is:",y)
+num1=int(input("please enter price of first item: "))
+num2=int(input("please enter price of second item: "))
+num3=num1+num2
+print("Total price of your groceries:",num3)
